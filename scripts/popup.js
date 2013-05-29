2835c22b3e286920d2f7286b45d57a74102a5173
anikitos <sanket.p25@gmail.com>
2013-05-28 20:14:04 -0400

    shows new posts, needs links and tags

---
+++ scripts/popup.js
@@ -0,0 +1,46 @@
+function fetch_feed() {
+ 
+	var req = new XMLHttpRequest();
+	req.open("GET", 'http://feeds.gawker.com/lifehacker/full', true);
+	req.onload = display_stories.bind(this);
+	req.send(null);
+	/*
+		chrome.extension.sendRequest({'action' : 'fetch_feed', 'url' : 'http://feeds.gawker.com/lifehacker/full'}, 
+			function(response) {
+				display_stories(response);
+			}
+		);
+	*/
+}
+
+function display_stories(feed_data) {
+
+    var xml_doc = feed_data.target.responseXML;
+	$xml = $(xml_doc);
+    var items = $xml.find("item");
+	console.log(items[0]);
+    $('#popup').html('<img src="images/logo.png" id="logo" onclick="open_item(\'http://lifehacker.com/\'); window.close();" /><br clear="all" />');
+    items.each(function(index, element) {
+        var post = parse_post(element);
+		console.log(post);
+        var item = '';
+        var class2 = '';
+        if (index >= localStorage['unread_count']) {
+            // // console.log('visited');
+            item += '<div class="post read">';
+        }
+        else {
+            item += '<div class="post">'
+        }
+        item += '<span class="tag">' + post.tag + '</span>\
+                    <a href="' + post.url + '">\
+                        <div id="' + post.id + '" class="item" onclick="open_item(\'' + post.url + '\');">\
+                            <img src="' + post.img + '" width="107" height="60" />\
+                            <h4>' + post.title + '</h4>\
+                            <span class="description">' + post.description + '...</span>\
+                        </div>\
+                    </a>';
+        item += '</div>';
+        $('#popup').append(item);
+    });
+}
